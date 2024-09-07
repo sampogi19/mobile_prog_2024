b627ae7a4a56f59473128cafdff82a34fb6b9ad8
sampogi19 <2202741.ub.edu.ph>
2024-09-07 10:52:45 +0800

    Demo GIT Change

--- play_demogitchange.js
+++ play_demogitchange.js
@@ -1,9 +1,22 @@
 console.log("Demo GIT Change");
 
-// ADD, COMMIT, PUSH
+// CRATE NEW REPO ON GITHUB
 /*
 
-1. git add.
-2. git commit -m  "DEMO GIT Change"
+1. Create a new repository on GitHub
+2. Go to the  folder where your project is located
+3. Run git init
+4. Run git add .
+5. Run git commit -m "Initial Commit"
+6. Run git branch -M main
+7. Run git remote add origin <url>
+8. Run git push -u origin main
+*/
+
+// ADD/EDIT, COMMIT, PUSH
+/*
+
+1. git add .
+2. git commit -m "Demo GIT Change"
 3. git push
 */
